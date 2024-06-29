7b5183d733897d372148bc437f6fe21915fe57ec
Gregg Milligan II <milligangregg@gmail.com>
2024-06-29 09:36:30 -0400

    map job listings from data

--- react-crash-2024/src/components/HomeCards.jsx
+++ react-crash-2024/src/components/HomeCards.jsx
@@ -7,7 +7,7 @@ function HomeCards() {
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-            <Card>
+            <Card key='devs'>
                 <h2 className="text-2xl font-bold">For Developers</h2>
                 <p className="mt-2 mb-4">
                 Browse our React jobs and start your career today
@@ -19,7 +19,7 @@ function HomeCards() {
                 Browse Jobs
                 </a>
             </Card>
-            <Card bg='bg-indigo-100'>
+            <Card key='employers' bg='bg-indigo-100'>
                 <h2 className="text-2xl font-bold">For Employers</h2>
                 <p className="mt-2 mb-4">
                 List your job to find the perfect developer for the role
